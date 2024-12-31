38f4807e700e196ec5682ddddc2353a8dac44cf5
Dustin Getz <dustin.getz@gmail.com>
2024-12-31 15:34:14 -0500

    move wip demos to src-private

--- src/datagrid/parser.cljc
+++
@@ -1,3 +0,0 @@
-(ns datagrid.parser
-  (:require [clojure.spec.alpha :as s]
-            [clojure.string :as str]))
--- src/ui_demo/compile_css.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-npx tailwindcss --config tailwind.config.js -i ui_demo.css -o ../../resources/public/user/ui_demo.css $@
