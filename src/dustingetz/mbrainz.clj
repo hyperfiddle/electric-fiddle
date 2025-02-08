1c4d9dd7ad3965ee7b7346fbf60c6ff174e73e54
Dustin Getz <dustin.getz@gmail.com>
2025-02-08 08:55:56 -0500

    mbrainz: add more named entities for tests, upgrade datomic-contrib tests to this model

--- src/dustingetz/mbrainz.clj
+++ src/dustingetz/mbrainz.clj
@@ -7,13 +7,15 @@
 (def test-conn (delay (d/connect mbrainz-uri)))
 (def test-db (delay (d/db @test-conn)))
 (def lennon 527765581346058) ; datomic pro
+(def pour-lamour 17592186058336)
+(def cobblestone 17592186068764)
+(def yanne 778454232478138)
 
 (comment
-  (d/touch (d/entity @test-db 100))
-  := #:db{:id 100,
-          :ident :release/name,
-          :valueType :db.type/string,
-          :cardinality :db.cardinality/one,
-          :index true,
-          :fulltext true,
-          :doc "The name of the release"})
+  (d/touch (d/entity @test-db pour-lamour))
+  := {:db/id 17592186058336,
+      :abstractRelease/gid #uuid"f05a1be3-e383-4cd4-ad2a-150ae118f622",
+      :abstractRelease/name "Pour l’amour des sous / Parle au patron, ma tête est malade",
+      :abstractRelease/type :release.type/single,
+      :abstractRelease/artists #{#:db{:id 778454232478138} #:db{:id 580542139477874}},
+      :abstractRelease/artistCredit "Jean Yanne & Michel Magne"})
