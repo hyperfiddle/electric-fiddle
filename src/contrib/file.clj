49410d49b4dc448d17feb4cb837e42ca684fdeac
Dustin Getz <dustin.getz@gmail.com>
2025-02-24 10:00:26 -0500

    lift contrib namespaces to hf-contrib

--- src/contrib/file.clj
+++
@@ -1,8 +0,0 @@
-(ns contrib.file
-  (:require [clojure.java.io :as io]))
-
-(defn ensure-file [filepath]
-  (let [file (io/file filepath)]
-    (io/make-parents file)
-    (when-not (.exists file) (.createNewFile file))
-    file))
