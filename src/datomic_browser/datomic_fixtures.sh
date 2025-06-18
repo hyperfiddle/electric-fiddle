3e69e8a1eb44a184efd21c89940cb2b850451eeb
Geoffrey Gaillard <geoffrey.gaillard@hey.com>
2025-06-18 10:09:20 +0200

    [datomic-browser] fix mbrainz restore action running before datomic is ready
    Ensures datomic process is fully booted before calling restore-db. Otherwise on
    slow(er) machines the restore-db command can fail, causing user confusion:
    1. mbrainz dataset is not available even if the script seem to succeed;
    2. datomic process stays alive, conflicting with next readme instruction saying
    "run datomic".

--- src/datomic_browser/datomic_fixtures.sh
+++ src/datomic_browser/datomic_fixtures.sh
@@ -18,6 +18,12 @@ fi
 
 datomic-pro/bin/transactor config/samples/dev-transactor-template.properties >>datomic.log 2>&1 &
 datomic_transactor_pid=$!
+
+set +ex
+echo "waiting for Datomic port..."
+while ! timeout 1 bash -c "echo > /dev/tcp/localhost/4334 2> /dev/null" 2> /dev/null; do sleep 1; done
+set -ex
+
 # https://datomic.narkive.com/OUskfRdr/backup-error
 datomic-pro/bin/datomic restore-db file:`pwd`/mbrainz-1968-1973 datomic:dev://localhost:4334/mbrainz-1968-1973
 kill $datomic_transactor_pid
