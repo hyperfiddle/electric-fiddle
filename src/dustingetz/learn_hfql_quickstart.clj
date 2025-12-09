3180ceee1d170152a8dde16b4badd60330111db1
Geoffrey Gaillard <geoffrey.gaillard@hey.com>
2025-12-09 11:11:28 +0100

    Rewrite HFQL quickstart examples to work around temporary pull syntax inconsistencies

--- src/dustingetz/learn_hfql_quickstart.clj
+++ src/dustingetz/learn_hfql_quickstart.clj
@@ -32,7 +32,7 @@
                  java.io.File/.getAbsolutePath
                  type]}))
   (hfql/pull q)
-  := {'!x
+  := {!x
       {'java.io.File/.getName ".",
        'java.io.File/.getAbsolutePath "/Users/dustin/src/hf/monorepo/electric-fiddle/.",
        'type java.io.File}})
@@ -40,38 +40,38 @@
 (tests
   "HFQL recursive pull"
   ; List of files
-  (def q (hfql {!x [java.io.File/.getName
-                    {java.io.File/.listFiles {* 1}}]})) ; recursive pull syntax
+  (def q (hfql [java.io.File/.getName
+                {java.io.File/.listFiles {* 1}}] ; recursive pull syntax
+           !x))
   (time (hfql/pull q)) ; "Elapsed time: 0.856208 msecs"
 
-  (def q (hfql {(clojure.java.io/file ".")
-                [java.io.File/.getName
-                 {java.io.File/.listFiles {* ...}}]})) ; infinite pull
+  (def q (hfql [java.io.File/.getName
+                {java.io.File/.listFiles {* ...}}] ; infinite pull
+           (clojure.java.io/file ".")))
   (def xs (time (vec (hfql/pull q)))) ; "Elapsed time: 0.193417 msecs"
   (type xs) := clojure.lang.PersistentVector
   (type (first xs)) := clojure.lang.MapEntry
-  (first (first xs)) := '(clojure.java.io/file ".")
-  #_(clojure.pprint/pprint xs) ; todo can it work?
-  #_xs ; Error printing return value (StackOverflowError)
+  #_(clojure.pprint/pprint xs) ; todo can it work? ; G yes but it prints a lot.
+  xs
   )
 
 (tests
  "HFQL cardinality many"
  (def xs (take 3 (all-ns)))
- (def q (hfql {xs {* [type]}})) ; * means foreach
+ (def q (hfql {* [type]} xs)) ; * means foreach
  (hfql/pull q)
- := {'!xs [{'type clojure.lang.Namespace}
-           {'type clojure.lang.Namespace}
-           {'type clojure.lang.Namespace}]}
+ := [{'type clojure.lang.Namespace}
+     {'type clojure.lang.Namespace}
+     {'type clojure.lang.Namespace}]
 
  ; in Datomic pull, the * is implicit from the cardinality of the schema attribute
  ; Arbitrary objects do not have schema, so the programmer must inject that information
 
  ; We can also query the seq object, which is typically not what you want but
  ; it is well defined. Note here we pull `count ` an aggregating fn.
- (def q (hfql {xs [type count]})) ; no *, so we see the collection, not each element
+ (def q (hfql [type count] xs)) ; no *, so we see the collection, not each element
  (hfql/pull q)
- := {'xs {'type clojure.lang.LazySeq, 'count 3}})
+ := {'type clojure.lang.LazySeq, 'count 3})
 
 
 ; Ok lets query some progressively more interesting objects
@@ -80,33 +80,31 @@
  "HFQL over clojure.deps"
  (require '[clojure.tools.deps :as deps])
  (def x (deps/slurp-deps (clojure.java.io/file "deps.edn"))) ; a deep EDN document
- (hfql/pull (hfql {x [{:aliases [count keys]}]}))
- := {'x
-     {:deps
-      {'count 10,
-       'keys
-       '[org.clojure/clojure
-         org.clojure/tools.logging
-         com.hyperfiddle/electric
-         ring-basic-authentication/ring-basic-authentication
-         org.clojure/clojurescript
-         com.hyperfiddle/hyperfiddle-contrib
-         com.hyperfiddle/rcf
-         com.hyperfiddle/hyperfiddle
-         ch.qos.logback/logback-classic
-         ring/ring-core]}}})
+ (hfql/pull (hfql [{:aliases [count keys]}] x))
+ := {:deps
+     {'count 10,
+      'keys
+      '[org.clojure/clojure
+        org.clojure/tools.logging
+        com.hyperfiddle/electric
+        ring-basic-authentication/ring-basic-authentication
+        org.clojure/clojurescript
+        com.hyperfiddle/hyperfiddle-contrib
+        com.hyperfiddle/rcf
+        com.hyperfiddle/hyperfiddle
+        ch.qos.logback/logback-classic
+        ring/ring-core]}})
 
 (tests
  "HFQL over Java"
  (import '[java.lang.management ManagementFactory])
  (def os (ManagementFactory/getOperatingSystemMXBean))
- (hfql/pull (hfql {os [.getArch .getAvailableProcessors .getCpuLoad .getSystemCpuLoad type]}))
- := {'os
-     {'.getArch "aarch64",
-      '.getAvailableProcessors 20,
-      '.getCpuLoad _,
-      '.getSystemCpuLoad _,
-      'type com.sun.management.internal.OperatingSystemImpl}})
+ (hfql/pull (hfql [.getArch .getAvailableProcessors .getCpuLoad .getSystemCpuLoad type] os))
+ := {'.getArch "aarch64",
+     '.getAvailableProcessors 20,
+     '.getCpuLoad _,
+     '.getSystemCpuLoad _,
+     'type com.sun.management.internal.OperatingSystemImpl})
 
 (comment
   "HFQL over AWS"
@@ -114,10 +112,10 @@
   (add-lib 'com.cognitect.aws/api)
   (require '[cognitect.aws.client.api :as aws])
   (def !s3 (aws/client {:api :s3 :region "us-east-1"})) ; requires aws login
-  (hfql/pull (hfql {(aws/invoke !s3 {:op :ListBuckets}) [{:Buckets {* [:Name]}}]}))
+  (hfql/pull (hfql [{:Buckets {* [:Name]}}] (aws/invoke !s3 {:op :ListBuckets})))
   (def s3-index (clojure.datafy/datafy !s3))
   (keys s3-index)
-  (hfql/pull (hfql {s3-index [:api :region {:ops keys}]})) ; inline fn application at leaf
+  (hfql/pull (hfql [:api :region {:ops keys}] s3-index)) ; inline fn application at leaf
   )
 
 (comment
@@ -130,11 +128,11 @@
         (map #(JarFile. (clojure.java.io/file %))))))
 
   (hfql/pull
-    (hfql {(list-project-jars)
-           {* [str
-               .getName
-               .getVersion
-               .getManifest]}}))
+    (hfql {* [str
+              .getName
+              .getVersion
+              .getManifest]}
+      (list-project-jars)))
 
   (def xs (->> (list-project-jars)
             (filter #(clojure.string/includes? (str %) "clojure"))))
@@ -145,17 +143,17 @@
   "HFQL big complicated nested pull"
   ; More complicated object, this one is a collection represented as an indexed map
   (def xs (ns-publics (find-ns 'clojure.core))) ; returns a REPL-friendly indexed map, not a list
-  (hfql/pull (hfql {xs {* [type {val [type str]}]}})) ; iterate the map's mapentries
-  (hfql/pull (hfql {xs {vals {* [type str]}}})) ; iterate the map's vals
+  (hfql/pull (hfql {* [type {val [type str]}]} xs)) ; iterate the map's mapentries
+  (hfql/pull (hfql {vals {* [type str]}} xs)) ; iterate the map's vals
 
   ; bigger still
   (def xs (->> (all-ns) (filter #(clojure.string/starts-with? (ns-name %) "clojure"))))
   (count xs) := 120
   (defn var-arglists [!var] (->> !var meta :arglists seq pr-str))
-  (def q (hfql {xs
-                {* [ns-name
-                    {meta [:doc]}
-                    {ns-publics {vals {* [str
-                                          {meta [:doc]}
-                                          var-arglists]}}}]}}))
+  (def q (hfql {* [ns-name
+                   {meta [:doc]}
+                   {ns-publics {vals {* [str
+                                         {meta [:doc]}
+                                         var-arglists]}}}]}
+           xs))
   (time (hfql/pull q)))
