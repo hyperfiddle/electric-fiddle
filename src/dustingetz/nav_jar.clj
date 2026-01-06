086a7e7fb74237134ba3560ca8b1d9dc254317ac
Geoffrey Gaillard <geoffrey.gaillard@hey.com>
2026-01-06 15:57:16 +0100

    [demo] fix selection in jar explorer
    Selecting a JarEntry is TBD. Not sure how to do it yet.

--- src/dustingetz/nav_jar.clj
+++ src/dustingetz/nav_jar.clj
@@ -7,26 +7,21 @@
    [hyperfiddle.hfql2 :as hfql :refer [hfql]]
    [hyperfiddle.hfql2.protocols :as hfqlp]))
 
+(defn jar-by-path [jar-path-str] (JarFile. (clojure.java.io/file jar-path-str)))
+
 (defn list-project-jars []
   (let [cp (System/getProperty "java.class.path")]
     (->> (clojure.string/split cp #"[;:]")
       (filter #(.endsWith % ".jar"))
-      (map #(JarFile. (clojure.java.io/file %))))))
+      (map jar-by-path))))
 
 (defn path-filename [filepath-str]
   (when filepath-str (.getName (check (File. filepath-str))))) ; doesn't have to exist
 
-(defn jar-entries [!jar] (enumeration-seq (.entries !jar)))
+(defn jar-entries [!jar] (.entries !jar))
 (defn jar-manifest [!jar] (some->> !jar .getManifest .getMainAttributes .entrySet seq (into {})))
 (defn jar-filename [!jar] (path-filename (.getName !jar)))
 (defn jar-jmanifest-entries [!jar-manifest] (into {} (seq (.entrySet !jar-manifest))))
-
-(extend-type java.util.jar.JarFile$JarFileEntry
-  hfqlp/Identifiable (-identify [x] (-> x str))  ; wrong impl, must be replaced by a symbolic constructor call
-  hfqlp/Suggestable (-suggest [_] (hfql [.getName
-                                         .getSize
-                                         .getCompressedSize
-                                         type])))
 
 (extend-type java.util.jar.JarFile
   hfqlp/Identifiable (-identify [x] `(jar-by-path ~(.getName x)))
@@ -36,24 +31,27 @@
                                          {.getVersion [.major .minor .feature]}
                                          jar-entries])))
 
-(extend-type java.util.jar.Manifest
-  hfqlp/Suggestable (-suggest [_] (hfql [.getMainAttributes #_.getEntries type])))
+(defmethod hfqlp/-hfql-resolve `jar-by-path [[_ jar-path-str]] (jar-by-path jar-path-str))
 
 (extend-type java.util.jar.JarFile$JarFileEntry
-  hfqlp/Suggestable (-suggest [_] (hfql [.getName .getSize type])))
+  ;; hfqlp/Identifiable (-identify [x] ? ) ; FIXME Identity is (jar-path × entry name) but not clear how to refer to the selected parent jar in context.
+  hfqlp/Suggestable (-suggest [_] (hfql [.getName
+                                         .getSize
+                                         .getCompressedSize
+                                         type])))
+
+(extend-type java.util.jar.Manifest
+  hfqlp/Suggestable (-suggest [_] (hfql [.getMainAttributes type])))
 
 (extend-type java.util.jar.Attributes
   hfqlp/Suggestable (-suggest [_] (hfql [.values jar-jmanifest-entries type])))
 
-;; TODO: Crashes
-;; #?(:clj (extend-type java.util.jar.Attributes$Name
-;;           hfqlp/Identifiable (-identify [x] (str x))))
-
-(extend-type java.util.jar.Attributes$Name ; TODO implement Identifiable as a symbolic constructor
+(extend-type java.util.jar.Attributes$Name
+  hfqlp/Identifiable (-identify [obj] `(jar-attribute-name ~(str obj)))
   hfqlp/Suggestable (-suggest [_] (hfql [.toString])))
 
-(defmethod hfqlp/-hfql-resolve 'dustingetz.nav-jar/jar-by-path [[_ jar-path]]
-  (JarFile. (clojure.java.io/file jar-path)))
+(defmethod hfqlp/-hfql-resolve `jar-attribute-name [[_ jar-attribute-name-str]] (new java.util.jar.Attributes$Name jar-attribute-name-str))
 
 (def sitemap
   {`jar (hfql {(list-project-jars) {* [*]}})})
+
